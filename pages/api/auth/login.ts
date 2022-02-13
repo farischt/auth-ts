9c19b149bce38dc87e8b8336b5670c99dee33543
Faris Chtatou <fchtatou@dial-once.com>
2022-02-13 23:48:14 +0100

    check if user is verified or not

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -34,6 +34,9 @@ export default async function handler(
       } else if (!(await user.checkPassword(req.body.password))) {
         res.statusCode = 401
         return res.json({ error: "invalid_credentials" })
+      } else if(!user.verified) {
+        res.statusCode = 401
+        return res.json({ error: "invalid_credentials" })
       }
 
       const authToken = await Database.AuthToken.create({
