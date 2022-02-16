71ff37753064dde15625c279a936e1b6b889d87a
Faris Chtatou <fchtatou@dial-once.com>
2022-02-16 23:36:20 +0100

    static class for perfomance

--- server/mails/index.ts
+++ server/mails/index.ts
@@ -4,26 +4,24 @@ import * as fs from "fs"
 import * as path from "path"
 import CONFIG from "./config/config.json"
 
-class EmailApi {
-  declare transporter: nodemailer.Transporter
-  declare templateDir: string
-  declare shownEmail: string
+export default class EmailServer {
+  static readonly transporter = nodemailer.createTransport({
+    host: CONFIG.CREDENTIALS.HOST,
+    port: CONFIG.CREDENTIALS.PORT,
+    secure: CONFIG.CREDENTIALS.SECURE,
+    auth: {
+      user: CONFIG.CREDENTIALS.AUTH.USER,
+      pass: CONFIG.CREDENTIALS.AUTH.PASS,
+    },
+  })
 
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: CONFIG.CREDENTIALS.HOST,
-      port: CONFIG.CREDENTIALS.PORT,
-      secure: CONFIG.CREDENTIALS.SECURE,
-      auth: {
-        user: CONFIG.CREDENTIALS.AUTH.USER,
-        pass: CONFIG.CREDENTIALS.AUTH.PASS,
-      },
-    })
-    this.templateDir = path.resolve(process.cwd(), "server/mails/templates")
-    this.shownEmail = CONFIG.SHOWN_EMAIL
-  }
+  static readonly templateDir = path.resolve(
+    process.cwd(),
+    "server/mails/templates"
+  )
+  static readonly shownEmail = CONFIG.SHOWN_EMAIL
 
-  createMailOptions(to: string, subject: string, html: string) {
+  private static createMailOptions(to: string, subject: string, html: string) {
     return {
       from: this.shownEmail,
       to,
@@ -32,13 +30,18 @@ class EmailApi {
     }
   }
 
-  getTemplate(file: string) {
+  private static getTemplate(file: string) {
     const filePath = path.join(this.templateDir, file)
     const source = fs.readFileSync(filePath, "utf-8").toString()
     return handlebars.compile(source)
   }
 
-  async sendMail(email: string, context: any, file: string, subject: string) {
+  public static async sendMail(
+    email: string,
+    context: any,
+    file: string,
+    subject: string
+  ) {
     const template = this.getTemplate(file)
     const htmlToSend = template(context)
     const mailOptions = this.createMailOptions(email, subject, htmlToSend)
@@ -51,5 +54,3 @@ class EmailApi {
     }
   }
 }
-
-export default new EmailApi()
